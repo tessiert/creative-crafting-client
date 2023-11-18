9fc263632ea79272fad8064256f742320eebe815
Trace Tessier <trace.tessier@gmail.com>
2023-11-18 10:35:22 -0500

    Latest deployment build

--- src/app/shared/baseUrl.js
+++ src/app/shared/baseUrl.js
@@ -1,2 +1,3 @@
+// Dev and Prod server routes
 // export const baseUrl = 'http://localhost:8080/';
 export const baseUrl = 'https://us-central1-creative-crafting-server.cloudfunctions.net/creativeCraftingServer/';
